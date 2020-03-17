8eb23638c0401b12c3c27f157ca78e9c355616fd
Imtiaz Ali Baloch <imtiaz-ali-3612>
2020-03-17 17:39:07 +0500

    first commit

---
+++ NER.py
@@ -0,0 +1,82 @@
+# import spacy
+# n=spacy.load("en")
+#
+cv=open('Hitesh Kumar - Resume for Research Assistants - Lab Instructors.txt')
+text=cv.read()
+cv.close()
+#
+# doc=n(text)
+# e=[(x.text,x.label_) for x in doc.ents]
+# for i in e:
+#     print(i)
+
+# build matcher: dtsukiyama
+# spacy 1.9
+import spacy
+from spacy.matcher import Matcher
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
+
+
+def extract_name(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # First name and Last name are always Proper Nouns
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+
+    matcher.add('NAME', None, pattern)
+
+    matches = matcher(nlp_text)
+
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        return span.text
+
+import re
+from nltk.corpus import stopwords
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# Grad all general stop words
+STOPWORDS = set(stopwords.words('english'))
+
+# Education Degrees
+EDUCATION = [
+            'BE','B.E.', 'B.E', 'BS', 'B.S',
+            'ME', 'M.E', 'M.E.', 'MS', 'M.S',
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH',
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+        ]
+
+def extract_education(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # Sentence Tokenizer
+    nlp_text = [sent.string.strip() for sent in nlp_text.sents]
+
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            # Replace all special symbols
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(r'(((20|19)(\d{2})))'), edu[key])
+        if year:
+            education.append((key, ''.join(year[0])))
+        else:
+            education.append(key)
+    return education
+
+print(extract_name(text))
+print(extract_education(text))
